399ca7ef402c4845854c3adebb373c346f2acf17
Nayeem0011 <sknayeemislam384@gmail.com>
2026-07-17 03:11:19 +0600

    cryptex

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import AcquireCRX from "./components/acquireCRX/AcquireCRX"
 import CTABanner from "./components/catBanner/CTABanner"
 import CoreProducts from "./components/coreProducts/CoreProducts"
+import Footer from "./components/footer/Footer"
 import Header from "./components/header/Header"
 import Partners from "./components/partners/Partners"
 import Testimonials from "./components/testimonials/Testimonials"
@@ -16,6 +17,7 @@ const App = () => {
       <Partners />
       <Testimonials />
       <CTABanner />
+      <Footer />
     </div>
   )
 }
